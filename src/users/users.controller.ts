a03fba4dd7534ae8398056509f3ab9522f4c9f5b
ilyass <79806692+illyasshaddoud@users.noreply.github.com>
2024-02-05 18:51:12 +0100

    init

---
+++ src/users/users.controller.ts
@@ -0,0 +1,50 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  ValidationPipe,
+  HttpException,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import mongoose from 'mongoose';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  async create(@Body(ValidationPipe) createUserDto: CreateUserDto) {
+    const createdUser = await this.usersService.create(createUserDto);
+    return createdUser;
+  }
+
+  @Get()
+  findAll() {
+    return this.usersService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    const isValid = mongoose.Types.ObjectId.isValid(id);
+    if (!isValid) throw new HttpException('User not found', 404);
+    const user = await this.usersService.findOne(id);
+    if (!user) throw new HttpException('User not found', 404);
+    return user;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.usersService.update(+id, updateUserDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.usersService.remove(+id);
+  }
+}
---
+++ src/users/users.service.ts
@@ -0,0 +1,31 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './schemas/user.schema';
+import { InjectModel, MongooseModule } from '@nestjs/mongoose';
+import { Model, mongo } from 'mongoose';
+
+@Injectable()
+export class UsersService {
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+
+  create(createUserDto: CreateUserDto) {
+    return this.userModel.create(createUserDto);
+  }
+
+  findAll() {
+    return this.userModel.find();
+  }
+
+  findOne(id: string) {
+    return this.userModel.findById(id);
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
